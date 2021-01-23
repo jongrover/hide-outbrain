535c6e59a90033540ef35fd15e870f17364f0326
jongrover <hello@jonathangrover.com>
2021-01-23 13:17:23 +0800

    troubleshoot multi-site database issues

--- hide-outbrain.php
+++ hide-outbrain.php
@@ -17,14 +17,14 @@ define( 'MYPLUGIN_PATH', trailingslashit( plugin_dir_path(__FILE__) ) );
 define( 'MYPLUGIN_URL', trailingslashit( plugins_url('/', __FILE__) ) );
 
 // Plugin Settings Link
-function add_action_links ( $actions ) {
-  $mylinks = array(
-    '<a href="' . admin_url( 'admin.php?page=hideoutbrain-settings-page' ) . '">Settings</a>',
-  );
-  $actions = array_merge( $actions, $mylinks );
-  return $actions;
-}
-add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'add_action_links' );
+// function add_action_links ( $actions ) {
+//   $mylinks = array(
+//     '<a href="' . admin_url( 'admin.php?page=hideoutbrain-settings-page' ) . '">Settings</a>',
+//   );
+//   $actions = array_merge( $actions, $mylinks );
+//   return $actions;
+// }
+// add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'add_action_links' );
 
 // Settings Page & Sidebar Link
 require_once MYPLUGIN_PATH . '/settings.php';
--- store.php
+++ store.php
@@ -21,8 +21,9 @@ add_action( 'wp_enqueue_scripts','my_scripts_style');
 
 function execute_ajax() {
   global $wpdb;
-  global $table_prefix;
-  $table_name = $table_prefix."options";
+  // global $table_prefix;
+  // $table_name = $table_prefix."options";
+  $table_name = "wp_2_options";
   $nonce = check_ajax_referer( 'ajax_call_nonce', 'nonce' );
   if($nonce == true) {
     $result = $wpdb->get_results("SELECT * FROM $table_name WHERE option_name = 'hideoutbrain_settings_titles_field'");
